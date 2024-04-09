e3190b9fcbffe0b3b183fe335f826a2ae2aabf35
Scammaz <44886383+Scammaz@users.noreply.github.com>
2024-04-09 13:32:05 -0400

    a

--- megamek/src/megamek/utilities/RankingsDBAccessor.java
+++ megamek/src/megamek/utilities/RankingsDBAccessor.java
@@ -1,4 +1,3 @@
-package megamek.utilities;
 import java.io.File;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -12,23 +11,22 @@ import org.w3c.dom.NodeList;
 
 public class RankingsDBAccessor {
 
-
     private static String playerFileName = "data/ranking/playerRanking.xml";
-    private static String BotFileName = "data/ranking/botRanking.xml";
-    private  File xmlFile = new File(playerFileName);
 
-    public static void updatePlayer( String playerName, int newElo, int newRanking) throws Exception {
+    public static void updatePlayer(String playerName, int newElo, int newRanking) throws Exception {
         File xmlFile = new File(playerFileName);
         Document doc = loadXMLDocument(xmlFile);
         Element playerElement = getPlayerElementByName(doc, playerName);
 
         if (playerElement != null) {
             updatePlayerData(playerElement, newElo, newRanking);
-            saveXMLDocument(doc, xmlFile);
-            System.out.println("Player '" + playerName + "' updated successfully.");
         } else {
-            System.out.println("Player '" + playerName + "' not found.");
+            // Add player if not existing
+            playerElement = addNewPlayer(doc, playerName, newElo, newRanking);
         }
+
+        saveXMLDocument(doc, xmlFile);
+        System.out.println("Player '" + playerName + "' updated successfully.");
     }
 
     private static Document loadXMLDocument(File xmlFile) throws Exception {
@@ -52,6 +50,21 @@ public class RankingsDBAccessor {
     private static void updatePlayerData(Element playerElement, int newElo, int newRanking) {
         playerElement.getElementsByTagName("elo").item(0).setTextContent(String.valueOf(newElo));
         playerElement.getElementsByTagName("ranking").item(0).setTextContent(String.valueOf(newRanking));
+    }
+
+    private static Element addNewPlayer(Document doc, String playerName, int newElo, int newRanking) {
+        Element newPlayer = doc.createElement("player");
+        Element nameElement = doc.createElement("name");
+        nameElement.appendChild(doc.createTextNode(playerName));
+        newPlayer.appendChild(nameElement);
+        Element eloElement = doc.createElement("elo");
+        eloElement.appendChild(doc.createTextNode(String.valueOf(newElo)));
+        newPlayer.appendChild(eloElement);
+        Element rankingElement = doc.createElement("ranking");
+        rankingElement.appendChild(doc.createTextNode(String.valueOf(newRanking)));
+        newPlayer.appendChild(rankingElement);
+        doc.getDocumentElement().appendChild(newPlayer);
+        return newPlayer;
     }
 
     private static void saveXMLDocument(Document doc, File xmlFile) throws Exception {
